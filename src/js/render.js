f53594e265a12f90be5c1a058181497b4f82ace0
Bryan Loyson <loysonbryan@hotmail.com>
2018-09-11 16:03:00 +0200

    no changes

--- src/js/render.js
+++ src/js/render.js
@@ -3,11 +3,11 @@ var crel = require('crel')
 require('../css/main.css')
 
 function checkAnswer(btn, answer) {
-	btnclass = btn.classList
-	btn = btn.textContent
+	//btn = btn.textContent
 	btn = btn.replace(/\n|\r/g, "")
 	
 	if (btn == answer) {
+		return true
 		alert('Good Answer')
 	} else {
 		alert('Bad Answer')
@@ -15,13 +15,16 @@ function checkAnswer(btn, answer) {
 }
 
 function renderQuiz(quiz) {
+	var component = crel('div')
 	var answer = quiz.CorrectAnswer
 	var btn1 = crel('button', {class: 'pure-button pure-button-primary'}, quiz.A)
 	var btn2 = crel('button', {class: 'pure-button pure-button-primary'}, quiz.B)
 	var btn3 = crel('button', {class: 'pure-button pure-button-primary'}, quiz.C)
 	
 	btn1.addEventListener('click', function() {
-		checkAnswer(btn1, answer)
+		if (checkAnswer(btn1.textContent, answer) == true) {
+			
+		}
 	})
 	btn2.addEventListener('click', function() {
 		checkAnswer(btn2.textContent, answer)
@@ -30,7 +33,7 @@ function renderQuiz(quiz) {
 		checkAnswer(btn3.textContent, answer)
 	})
 		
-	return crel('div',
+	return crel(component,
 				crel('h3', quiz.Question),
 				btn1, btn2, btn3)
 }
