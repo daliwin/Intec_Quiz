47f3accb187f8c4338e8a4819e235ca3e729f582
Bryan Loyson <loysonbryan@hotmail.com>
2018-09-13 10:58:19 +0200

    button changes color if answer true or false

--- src/js/render.js
+++ src/js/render.js
@@ -2,15 +2,27 @@ var crel = require('crel')
 
 require('../css/main.css')
 
+function toggleColorC(btn) {
+	var buttonClass = btn.classList
+	buttonClass.remove('button-secondary', 'pure-button')
+	buttonClass.add('button-success', 'pure-button')
+}
+
+function toggleColorW(btn) {
+	var buttonClass = btn.classList
+	buttonClass.remove('button-secondary', 'pure-button')
+	buttonClass.add('button-error', 'pure-button')
+}
+
 function checkAnswer(btn, answer) {
-	//btn = btn.textContent
-	btn = btn.replace(/\n|\r/g, "")
+	btn1 = btn.textContent
+	btn1 = btn1.replace(/\n|\r/g, "")
 	
-	if (btn == answer) {
-		alert('Good Answer')
+	if (btn1 == answer) {
+		toggleColorC(btn)
 		return true
 	} else {
-		alert('Bad Answer')
+		toggleColorW(btn)
 		return false
 	}
 }
@@ -18,18 +30,18 @@ function checkAnswer(btn, answer) {
 function renderQuiz(quiz) {
 	var component = crel('div')
 	var answer = quiz.CorrectAnswer
-	var btn1 = crel('button', {class: 'pure-button pure-button-primary'}, quiz.A)
-	var btn2 = crel('button', {class: 'pure-button pure-button-primary'}, quiz.B)
-	var btn3 = crel('button', {class: 'pure-button pure-button-primary'}, quiz.C)
+	var btn1 = crel('button', {class: 'button-secondary pure-button'}, quiz.A)
+	var btn2 = crel('button', {class: 'button-secondary pure-button'}, quiz.B)
+	var btn3 = crel('button', {class: 'button-secondary pure-button'}, quiz.C)
 	
 	btn1.addEventListener('click', function() {
-		checkAnswer(btn1.textContent, answer)
+		checkAnswer(btn1, answer)
 	})
 	btn2.addEventListener('click', function() {
-		checkAnswer(btn2.textContent, answer)
+		checkAnswer(btn2, answer)
 	})
 	btn3.addEventListener('click', function() {
-		checkAnswer(btn3.textContent, answer)
+		checkAnswer(btn3, answer)
 	})
 		
 	return crel(component,
